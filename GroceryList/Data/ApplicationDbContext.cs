b2736771a1accde33dfe5f8fa795844b6ac7518f
hphifer <hphife@yahoo.com>
2021-03-19 14:57:59 -0400

    Created App Added service in start up Created model Created DBContext Added nuget packages

---
+++ GroceryList/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GroceryList.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GroceryList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Items> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ GroceryList/Models/Items.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GroceryList.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+
+    }
+}
